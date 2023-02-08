8344bc0efe94f8fbc2d64df72b303fc67bbfa356
GeorgeZ1917 <jorge.z2020@outlook.com>
2023-02-07 18:49:55 -0600

    Initial version.

---
+++ Definitions.cs
@@ -0,0 +1,241 @@
+﻿    // Definitions.cs
+
+
+using System;
+
+
+public class MyClass
+{
+    private int val;
+    public MyClass()
+    {
+        ;
+    }
+    public MyClass(int i)
+    {
+        val = i;
+    }
+    public void setVal(int i)
+    {
+        val = i;
+    }
+    public void getVal()
+    {
+        Console.WriteLine("The value of val is {0}", this.val + ".");
+    }
+    public int getVal(int valX)
+    {
+        return val;
+    }
+    public static void SelectionSort(int[] array)
+    {
+        int min, temp;
+        for (int i = 0; i < array.Length; i++)
+        {
+            min = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[min])
+                {
+                    min = j;
+                }
+            }
+            temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+
+    public static void InsertionSort(int[] array)
+    {
+        int i, key;
+        for (int j = 1; j < array.Length; j++)
+        {
+            key = array[j];
+            i = j;
+            while (i > 0 && (key < array[i - 1]))
+            {
+                array[i] = array[i - 1];
+                i--;
+            }
+            array[i] = key;
+        }
+    }
+
+    public static void printArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element.ToString() + " ");
+        }
+        Console.WriteLine("");
+    }
+    public static void testData()
+    {
+        //System.Int32 intObj = new System.Int32();
+        //intObj = 50;
+        string strObj = "String object.";
+        Console.WriteLine("The minimum value of intObj is {0} ", int.MinValue);
+        Console.WriteLine("The value of strObj is {0} ", strObj);
+    }
+    public virtual void virtMeth()
+    {
+        Console.WriteLine("val: " + val.ToString());
+    }
+    public static int [] returnArray()
+    {
+        int [] newArray = new int [] {2, 7, 9, 2, 10, 2, 2, 16, 90, 61, 7, 1, 6, 8, 7, 4};
+        return newArray;
+    }
+    public virtual void derivedClassMethod()
+    {
+        Console.WriteLine("Default virtual implementation.");
+    }
+}
+
+public class DerivedClass : MyClass
+{
+    private int valX;
+    public DerivedClass(int i)
+    {
+        valX = i;
+    }
+    public override void virtMeth()
+    {
+        Console.WriteLine("valX * valX: " + (valX * valX).ToString());
+    }
+}
+
+public class DerivedClassTwo : MyClass
+{
+    public float dataDerived;
+    public override void derivedClassMethod()
+    {
+        Console.WriteLine("Inside derived method from DerivedClassTwo");
+    }
+}
+
+public class DerivedClassThree : MyClass
+{
+    public float dataDerived;
+    public override void derivedClassMethod()
+    {
+        Console.WriteLine("Inside derived method from DerivedClassThree");
+    }
+}
+
+
+public class MethodsClass
+{
+    public static void partialParams (int val, string myString, params float [] vals)
+    {
+        Console.WriteLine("val squared: {0}", val * val);
+        Console.WriteLine("myString expanded: " + myString + ", and that's all, folks!");
+        Console.Write("The params arguments: ");
+        foreach (float num in vals)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine("\n");
+    }
+    public static void optionalParams (ref int val, ref MyClass obj, bool flag = false)
+    {
+        val *= val;
+        obj.setVal(10);
+        flag = true;
+    }
+    public static (int val, string stringVar) returnTuple()
+    {
+        return (42, "Tuple string");
+    }
+}
+
+
+public class PropertiesClass
+{
+    private int intData;
+    private string stringData;
+    private bool boolData;
+    public int intProp
+    {
+        get { return intData; }
+        set 
+        {
+            if (value > 10_000)
+            {
+                throw new Exception ("Cannot set the value; it is greater than 10,000.");
+            }
+            else
+            {
+                intData = value;
+            }
+        }
+    }
+    public string stringProp
+    {
+        get { return stringData; }
+        set { stringData = value; }
+    }
+    public bool boolProp { get; set; }
+    public PropertiesClass ()
+    {
+        readVar = 3.141592f;
+    }
+    public PropertiesClass (int dataInt, string dataString, bool dataBool)
+    {
+        intData = dataInt;
+        stringData = dataString;
+        boolData = dataBool;
+    }
+    public readonly float readVar;
+}
+
+
+
+public class Node
+{
+    public int data;
+    public Node next;
+    public static void insertElement(Node head, int Data)
+    {
+        Node newNode = new Node();
+        newNode.data = Data;
+        newNode.next = head.next;
+        head.next = newNode;
+    }
+    public static void printList(Node head)
+    {
+        Node currentNode = head;
+        int i = 0;
+        while (currentNode != null)
+        {
+            Console.WriteLine("Data of node {0}: {1}.", i, currentNode.data);
+            i ++;
+            currentNode = currentNode.next;
+        }
+    }
+    public static Node findNode(Node head, int Data)
+    {
+        Node temp = head;
+        Node currentNode = head;
+        if(temp.data == Data && temp.next == null)
+        {
+            head = head.next;
+            return temp;
+        }
+        else
+        {
+            while(temp.next != null)
+            {
+                if(temp.next.data == Data)
+                {
+                    currentNode = temp.next;
+                    temp.next = temp.next.next;
+                    return currentNode;
+                }
+                temp = temp.next;
+            }
+            return null;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,165 @@
+//Main.cs
+
+using System;
+using System.Collections.Generic;
+
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World");
+        int[] numbers1 = { 1, 3, 0, 0, -4, 10, 5, 3, 9, -7, -7, 5, 1, 0, -10 };
+        int[] numbers2 = { 1, 3, 0, 0, -4, 10, 5, 3, 9, -7, -7, 5, 1, 0, -10 };
+        MyClass.SelectionSort(numbers1);
+        MyClass.printArray(numbers1);
+        MyClass.InsertionSort(numbers2);
+        MyClass.printArray(numbers2);
+
+        Random r = new Random();
+
+        int[] longArray = new int[100];
+        for (int i = 0; i < 100; i++)
+        {
+            longArray[i] = r.Next(-100, 100);
+        }
+
+        MyClass.SelectionSort(longArray);
+        MyClass.printArray(longArray);
+
+        MyClass obj = new MyClass(5);
+        obj.getVal();
+        obj.setVal(10);
+        obj.getVal();
+
+        MyClass.testData();
+
+        String aString = "The fisrt string";
+        String anotherString = "The second string";
+
+        Console.WriteLine("Are the strings equal? {0}", aString.Equals(anotherString));
+
+        DerivedClass derObj = new DerivedClass(5);
+        MyClass myObj = new MyClass(3);
+        derObj.virtMeth();
+        myObj.virtMeth();
+        Console.WriteLine("Example values: {0:f}, {1:n}, {2:n}", 1050, 3014, 1070004);
+        string myString = "Example string";
+        string anotherStringTwo = "Another string";
+        string sameString = "Example string";
+        int anInt = 123456789;
+        int anotherInt = 987654321;
+        int sameInt = 987654321;
+        Console.WriteLine("myString equals anotherStringTwo: {0}", myString == anotherStringTwo);
+        Console.WriteLine("myString equals anotherStringTwo: {0}", myString.Equals(anotherStringTwo));
+        Console.WriteLine("myString equals sameString: {0}", myString.Equals(sameString));
+        Console.WriteLine("myString without \'r\': {0}", myString.Replace("r", ""));
+        Console.WriteLine("anInt is equal to anotherInt: {0}", anInt.Equals(anotherInt));
+        Console.WriteLine("anotherInt is equal to sameInt: {0}", anotherInt.Equals(sameInt));
+        foreach (char val in myString)
+        {
+            Console.WriteLine(val);
+        }
+        
+        List <double> doubleList = new List<double> ();
+        for (int i = 0; i < 10; i ++ )
+        {
+            doubleList.Add((double) i * i * 0.50);
+        }
+        foreach ( double val in doubleList)
+        {
+            Console.Write(val + " ");
+        }
+        Console.WriteLine("\n");
+        foreach (double val in doubleList)
+        {
+            switch ((int) val)
+            {
+                case 2:
+                    Console.WriteLine("The value is 2.");
+                    break;
+                case 8:
+                    Console.WriteLine("The value is 8.");
+                    break;  
+                case 18:
+                case 32:
+                    Console.WriteLine("The value is either 18 or 32.");
+                    break;                    
+            }
+        }
+        int [] myArray = new int [] {1, 6, 0, 0, 4, 3, 7, 9, 1, 10};
+        Array.Sort(myArray);
+        foreach (int val in myArray)
+        {
+            Console.Write(val + " ");
+        }
+        Console.WriteLine("\n");
+        int [] theArray = MyClass.returnArray();
+        Array.Sort(theArray);
+        foreach (int val in theArray)
+        {
+            Console.Write(val + " and ");
+        }
+        MethodsClass.partialParams(3, "A string", 3.1415f, 2.718f);
+        MyClass newObjMyClass = new MyClass (5);
+        bool theFlag = false;
+        int refInt = 8;
+        MethodsClass.optionalParams (ref refInt, ref newObjMyClass);
+        Console.WriteLine("refInt after reference passing: {0}.", refInt);
+        Console.WriteLine("newObjMyClass.val after reference passing: {0}.", newObjMyClass.getVal(5));
+        MethodsClass.optionalParams (ref refInt, ref newObjMyClass, theFlag);
+
+        Node headNode = new Node();
+        headNode.next = null;
+        headNode.data = 5;
+
+        Node.insertElement(headNode, 2);
+        Node.insertElement(headNode, 1);
+        Node.insertElement(headNode, 7);
+        Node.insertElement(headNode, 9);
+        Node.insertElement(headNode, 12);
+        Node.insertElement(headNode, 6);
+        Node.insertElement(headNode, 20);
+        var rand = new Random();
+        for (int i = 0; i < 1_000_000; i ++)
+        {
+            Node.insertElement(headNode, rand.Next(0, 1_000_000));
+        }
+        //Node.printList(headNode);
+
+        Node newNode = Node.findNode(headNode, 7);
+
+        Console.WriteLine("Value of newNode.data is {0}.", newNode.data);
+        //Node.printList(headNode);
+
+        (int intVal, float floatVal, string stringVal) myTuple = (42, 3.1415f, "Hello World.");
+        Console.WriteLine("The float value of myTuple is {0}:", myTuple.floatVal);
+        (int val, string theString) tupleFromMethod = MethodsClass.returnTuple();
+        Console.WriteLine("The string value of tupleFromMethod is: {0}", tupleFromMethod.theString);
+
+
+        MyClass myClassTwo = new DerivedClassTwo();
+        MyClass myClassThree = new DerivedClassThree();
+        myClassTwo.derivedClassMethod();
+        myClassThree.derivedClassMethod();
+
+
+        PropertiesClass propObj = new PropertiesClass();
+        PropertiesClass propObj2 = new PropertiesClass(42, "Hello, World!", false);
+        try
+        {
+            propObj.intProp = 50_00;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Error setting the integer data: " + ex.Message);
+        }
+        propObj.stringProp = "String data.";
+        propObj.boolProp = true;
+        Console.WriteLine("Integer value of propObj: {0}, string value of propObj: '{1}', bool value of objProp: {2}.", propObj.intProp, propObj.stringProp, propObj.boolProp);
+        Console.WriteLine("Integer value of propObj2: {0}, string value of propObj2: '{1}', bool value of propObj2: {2}.", propObj2.intProp, propObj2.stringProp, propObj2.boolProp);
+        Console.WriteLine("Readonly value from PropertiesClass: {0}.", propObj.readVar.ToString());
+        
+        Console.ReadKey();
+    }
+}
